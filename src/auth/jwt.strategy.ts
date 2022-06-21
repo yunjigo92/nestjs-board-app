f6ab3f159c3c436606406095eb1b7f44075e8650
yj.ko <yunjigo92@gmail.com>
2022-06-21 12:30:25 +0900

    jwt 인증 추가

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,1 +1,16 @@
-test
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Strategy } from "passport-jwt";
+import { UserRepository } from "./user.repository";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository
+    ){
+        
+    }
+    
+}
